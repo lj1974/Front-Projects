2b9138654d072267e57bf9b2ebc4a751bd17724d
LJ <84136544+lj1974@users.noreply.github.com>
2021-11-27 17:53:36 -0300

    EA BD finished

--- Personals/EA_BD/eainte.php
+++ Personals/EA_BD/eainte.php
@@ -12,7 +12,7 @@ $place = $_POST['country'];
 $skill = $_POST['ability'];
 $ocupation = $_POST['ocupation'];
 
-// $orga = $_POST['organization'];
+$orga = $_POST['organization'];
 
 $title = $_POST['titles'];
 $desc = $_POST['desc'];
@@ -24,8 +24,14 @@ if(empty($_POST['name'])){
 } else {
 
     $sql = "insert into personagem(nome, alinhamento, pais, habilidade, ocupacao, titulo, descricao_perso, age)
-    values('$nome', '$align', '$place', '$skill', '$ocupation', '$title', '$desc', '$age')";
+    values('$nome', '$align', '$place', '$skill', '$ocupation', '$title', '$desc', '$age', '$orga')";
     $result = mysqli_query($conn, $sql);
+
+    $take_perso = "select idperso from personagem where idperso = '$nome'";
+    echo "$take_perso";
+
+    $sql_org = "insert into org_perso(org_id_take, perso_id_take) values ('$orga', '$take_perso')";
+    $result2 = mysqli_query($conn, $sql_org);
 
 
 if (!$sql){
